15dbefd2fa98d5be9eb4bd84f6add38d841ff0ca
chaosboy99 <brayanguanangamorales@gmail.com>
2024-06-23 21:29:51 +0200

    enemigoMov

--- juegoPolyBry/Assets/Scripts/Enemigo2D.cs
+++ juegoPolyBry/Assets/Scripts/Enemigo2D.cs
@@ -1,22 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Enemigo2D : MonoBehaviour
 {
+    [SerializeField] private float Speed;
     [SerializeField] private Transform Pj;
+    [SerializeField] public Animator Ani;
 
     private bool MirandoR;
+    private int hitCount = 0;
+    private const int maxHits = 3;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        
+        
+    }
+    void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, Pj.position, Speed * Time.deltaTime);
+        Ani.SetBool("Run", true);
         bool isPlayerR = transform.position.x < Pj.transform.position.x;
         Flip(isPlayerR);
-        
+
     }
 
     private void Flip(bool isPlayerR)
@@ -27,16 +40,18 @@ public class Enemigo2D : MonoBehaviour
             Vector3 scale = transform.localScale;
             scale.x *= -1;
             transform.localScale = scale;
+            
 
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ReceiveHit()
     {
-        
+        hitCount++;
+        if (hitCount >= maxHits)
+        {
+            Destroy(gameObject);
+        }
     }
 
-   
-   
+
 }
